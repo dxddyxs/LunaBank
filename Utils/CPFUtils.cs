efbac05bf1194dedb4cd9c5190cbf2e049d24fc6
dxddyxs <derecklisboaj3@gmail.com>
2025-11-23 19:34:44 -0300

    feat: Add CPF validation utility.

---
+++ Utils/CPFUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunaBank.Utils
+{
+    public static class CPFUtils
+    {
+        public static bool IsValid(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+                return false;
+
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            return cpf.Length == 11;
+        }
+    }
+}
