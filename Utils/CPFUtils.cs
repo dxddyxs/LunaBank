02be372a607a2b6a856dfff44720920e4f2f3f82
dxddyxs <derecklisboaj3@gmail.com>
2025-11-23 20:08:33 -0300

    feat: Improve CPF validation logic and add edge case checks

--- Utils/CPFUtils.cs
+++ Utils/CPFUtils.cs
@@ -11,9 +11,29 @@ namespace LunaBank.Utils
             if (string.IsNullOrEmpty(cpf))
                 return false;
 
-            cpf = cpf.Replace(".", "").Replace("-", "");
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
 
-            return cpf.Length == 11;
+            if (cpf.Length != 11)
+                return false;
+
+            if (cpf.All(c => c == cpf[0]))
+                return false;
+
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+                sum += (cpf[i] - '0') * (10 - i);
+
+            int firstDigit = 11 - (sum % 11);
+            if (firstDigit > 9) firstDigit = 0;
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+                sum += (cpf[i] - '0') * (11 - i);
+
+            int secondDigit = 11 - (sum % 11);
+            if (secondDigit > 9) secondDigit = 0;
+
+            return (cpf[9] - '0' == firstDigit) && (cpf[10] - '0' == secondDigit);
         }
     }
 }
