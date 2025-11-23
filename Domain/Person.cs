4b9934462b6cfa5f37eee4e07ff7a8c23474f9db
derec <derec@DESKTOP-D4JLCGU>
2025-11-23 19:28:10 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunaBank.Domain
+{
+    public class Person
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+    }
+}
