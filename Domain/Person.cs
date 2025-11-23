fbc3e3faef164b27a999014d41375a09d1f7d569
dxddyxs <derecklisboaj3@gmail.com>
2025-11-23 19:34:40 -0300

    feat: Add Person entity with validation and constructor

--- Domain/Person.cs
+++ Domain/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LunaBank.Utils;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,27 @@ namespace LunaBank.Domain
     public class Person
     {
         public Guid Id { get; } = Guid.NewGuid();
-        public string Nome { get; private set; }
+        public string Name { get; private set; }
         public string CPF { get; private set; }
+
+        public Person(string name, string cpf)
+        {
+            SetName(name);
+            SetCPF(cpf);
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be null or empty.");
+            Name = name;
+        }
+
+        public void SetCPF(string cpf)
+        {
+            if (!CPFUtils.IsValid(cpf))
+                throw new ArgumentException("Invalid CPF.");
+            CPF = cpf;
+        }
     }
 }
